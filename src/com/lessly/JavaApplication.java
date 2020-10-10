b52ba15a9a96cc928339d1ce7e5834ad491d4e52
lessly-hithub <jayakumar.lessly@hotmail.com>
2020-10-10 21:29:06 +0530

    My Initial Commit

---
+++ src/com/lessly/JavaApplication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lessly;
+
+/**
+ * @author lessly
+ *
+ */
+public class JavaApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Docker Project Integration with Jenkins.");
+
+	}
+
+}
